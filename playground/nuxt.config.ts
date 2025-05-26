6760493658ecbd57cfb5df15d426c8be506b990b
Halit Sever <halitsever@protonmail.com>
2025-05-26 10:29:52 +0300

    chore: add compatibilityDate to nuxt.config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   modules: ["../src/module"],
   devtools: { enabled: true },
+  compatibilityDate: '2025-05-26',
   runtimeConfig: {
     TELEGRAM_TOKEN: "my_fancy_bot_token",
   },
